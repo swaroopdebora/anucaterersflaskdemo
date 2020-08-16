798df7805ba6c5e61d9c0eb7594afad687c4bf23
swaroopdebora <kumar11.2k@gmail.com>
2020-08-16 22:52:35 +0000

    merged a flask app from another repo

--- mongo.py
+++ mongo.py
@@ -4,28 +4,51 @@ import os
 
 MONGODB_URI = "mongodb://root:RootUser@myfirstcluster-shard-00-00.zhfps.mongodb.net:27017,myfirstcluster-shard-00-01.zhfps.mongodb.net:27017,myfirstcluster-shard-00-02.zhfps.mongodb.net:27017/myTestDB?ssl=true&replicaSet=atlas-z7d5ni-shard-0&authSource=admin&retryWrites=true&w=majority"
 DBS_NAME = "myTestDB"
-COLLECTION_NAME = "breakfast"
+COLLECTION_NAME1 = "breakfast"
+COLLECTION_NAME2 = "locations"
 
 
 def mongo_connect(url):
     try:
         conn = pymongo.MongoClient(url)
-        print("Mongo is connected!")
         return conn
     except pymongo.errors.ConnectionFailure as e:
         print("Could not connect to MongoDB: %s") % e
-        
+
 
 conn = mongo_connect(MONGODB_URI)
+coll1 = conn[DBS_NAME][COLLECTION_NAME1]
 
-coll = conn[DBS_NAME][COLLECTION_NAME]
+coll2 = conn[DBS_NAME][COLLECTION_NAME2]
 
-new_doc = [{'category': 'veg_menu', 'name': 'sambar rice', 'quantity': '170 grams', 'price': '3 dollars', 'calories': '300'}, {'category': 'veg_menu', 'name': 'curd rice', 'quantity': '170 grams', 'price': '3 dollars', 'calories': '200'}, {'category': 'veg_menu', 'name': 'coconut rice', 'quantity': '170 grams', 'price': '3 dollars', 'calories': '300'}, {'category': 'veg_menu', 'name': 'veg pulao', 'quantity': '170 grams', 'price': '3 dollars', 'calories': '300'},{'category': 'veg_menu', 'name': 'tomato rice', 'quantity': '170 grams', 'price': '3 dollars', 'calories': '300'}, {'category': 'veg_menu', 'name': 'pulihora', 'quantity': '170 grams', 'price': '3 dollars', 'calories': '300'}]
-coll.insert_many(new_doc)
 
-#coll.update_many({'category':'non_veg_menu'},{'$set':{'price':'6 dollars'}})
+def find_menu(category):
+    docs = coll1.find({'category': category})
+    x = []
+    for i in docs:
+        x.append(i)
+    for item in x:
+        print(item["name"]+" -  price is "+item["price"]+" and calories is "+item["calories"])
 
-documents = coll.find()
 
-for doc in documents:
-    print(doc)
+#find_menu("sweets")
+
+def find_locations():
+    docs = coll2.find()
+    x=[]
+    for i in docs:
+        x.append(i)
+    for item in x:
+        print(item["location"]+" - timings are from "+item["timings"]+" , Dining Available ?- "+item["dineavailable"])
+
+
+#find_locations()
+
+def update_locations():
+    listoflocations1 = ["Brampton","Missasaga","Toronto"]
+    listoflocations2 = ["Brampton","Missasaga","Toronto","Hamilton","North York"]
+    docs = coll1.update_many({'category':'veg_menu'},{'$set':{'location':listoflocations2}})
+    print("hi")
+    
+
+update_locations()
---
+++ mongo_menu.py
@@ -0,0 +1,136 @@
+import pymongo
+import os
+
+MONGODB_URI = "mongodb://root:RootUser@myfirstcluster-shard-00-00.zhfps.mongodb.net:27017,myfirstcluster-shard-00-01.zhfps.mongodb.net:27017,myfirstcluster-shard-00-02.zhfps.mongodb.net:27017/myTestDB?ssl=true&replicaSet=atlas-z7d5ni-shard-0&authSource=admin&retryWrites=true&w=majority"
+DBS_NAME = "myTestDB"
+COLLECTION_NAME = "breakfast"
+
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected!")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+
+
+def get_record():
+    print("")
+    name = input("Enter item> ")
+    try:
+        doc = coll.find_one({'name': name.lower()})
+    except:
+        print("Error accessing the database")
+    
+    if not doc:
+        print("")
+        print("Error! No results found.")
+    
+    return doc
+
+def show_menu():
+    print("")
+    print("1. Add a new dish")
+    print("2. Find an dish by name")
+    print("3. Edit a dish")
+    print("4. Delete a dish")
+    print("5. Exit")
+
+    option = input("Enter option: ")
+    return option
+
+def add_record():
+    print("")
+    category = input("Enter category> ")
+    name = input("Enter name > ")
+    quantity = input("Enter quantity > ")
+    price = input("Enter price > ")
+    calories = input("Enter calories > ")
+    
+    new_doc = {'category': category.lower(), 'name': name.lower(), 'quantity': quantity,
+               'price': price, 'calories': calories}
+    
+    try:
+        coll.insert(new_doc)
+        print("")
+        print("Document inserted")
+    except:
+        print("Error accessing the database")
+
+
+def find_record():
+    doc = get_record()
+    if doc:
+        print("")
+        for k, v in doc.items():
+            if k != "_id":
+                print(k.capitalize() + ": " + v.capitalize())
+
+
+def edit_record():
+    doc = get_record()
+    if doc:
+        print("Hi")
+        update_doc = {}
+        print("")
+        for k, v in doc.items():
+            if k != "_id":
+                update_doc[k] = input(k.capitalize() + " [" + v + "] > ")
+
+                if update_doc[k] == "":
+                    update_doc[k] = v
+        
+        try:
+            coll.update_one(doc, {'$set': update_doc})
+            print("")
+            print("Document updated")
+        except:
+            print("Error accessing the database")
+
+
+def delete_record():
+
+    doc = get_record()
+
+    if doc:
+        print("")
+        for k, v in doc.items():
+            if k != "_id":
+                print(k.capitalize() + ": " + v.capitalize())
+        
+        print("")
+        confirmation = input("Is this the document you want to delete?\nY or N > ")
+        print("")
+
+        if confirmation.lower() == 'y':
+            try:
+                coll.remove(doc)
+                print("Document deleted!")
+            except:
+                print("Document not deleted")
+
+
+def main_loop():
+    while True:
+        option = show_menu()
+        if option == "1":
+            add_record()
+        elif option == "2":
+            find_record()
+        elif option == "3":
+            edit_record()
+        elif option == "4":
+            delete_record()
+        elif option == "5":
+            conn.close()
+            break
+        else:
+            print("Invalid option")
+        print("")
+
+
+conn = mongo_connect(MONGODB_URI)
+coll = conn[DBS_NAME][COLLECTION_NAME]
+
+main_loop()
